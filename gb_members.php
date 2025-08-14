630d5d85b0228a8fcec3fcd006f29e6c03362806
OkArafat <arafatbiswas.edu01@gmail.com>
2025-08-14 13:35:13 +0600

    changes

--- gb_members.php
+++ gb_members.php
@@ -116,7 +116,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'gas_proxy') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GB Members - Styled</title>
+    <title>GB Members - BUCuC</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
